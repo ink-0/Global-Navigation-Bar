9857e4f3e27b3616775505b25c55c0e193949d3e
ink-0 <71919983+ink-0@users.noreply.github.com>
2021-07-20 21:00:33 +0900

    UI: NavBarUI 구성
    - NavLogo 생성
    - NavMenu 생성
    - mock data 생성및 메뉴 뿌려주기
    - ink-0/global-navigation-bar/#1

--- global-navigation-bar/src/components/NavAccount.jsx
+++ global-navigation-bar/src/components/NavAccount.jsx
@@ -1,5 +1,47 @@
+import styled from 'styled-components';
+import AccountSearchIcon from '../icons/AccountSearchIcon';
+import AccountAlarmIcon from '../icons/AccountAlarmIcon';
+import AccountUserImg from './AccountUserImg';
+import AccountTag from './AccountTag';
+import AccountHamburgerIcon from '../icons/AccountHamburgerIcon';
 const NavAccount = () => {
-  return <></>;
+  return (
+    <NavAccountDiv>
+      <NavAccountUl>
+        <NavAccountLi>
+          <AccountSearchIcon />
+        </NavAccountLi>
+        <NavAccountLi>
+          <AccountAlarmIcon />
+        </NavAccountLi>
+        <NavAccountLi>
+          <AccountUserImg />
+        </NavAccountLi>
+        <NavAccountLiBar>
+          <AccountTag />
+        </NavAccountLiBar>
+      </NavAccountUl>
+    </NavAccountDiv>
+  );
 };
 
 export default NavAccount;
+const NavAccountDiv = styled.div``;
+const NavAccountUl = styled.ul`
+  display: flex;
+  list-style: none;
+  align-items: center;
+`;
+const NavAccountLi = styled.li`
+  display: flex;
+  padding: 0 10px;
+`;
+const NavAccountLiBar = styled(NavAccountLi)`
+  ::before {
+    content: '';
+    width: 1px;
+    height: 10px;
+    background-color: #e1e2e3;
+    margin: auto 10px;
+  }
+`;
